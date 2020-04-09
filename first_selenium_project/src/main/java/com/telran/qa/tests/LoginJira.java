cee0f1d779a41be6cec6150861795cb75410ab15
ninavasil <62669724+ninavasil@users.noreply.github.com>
2020-04-09 11:46:22 +0300

    added method type

--- first_selenium_project/src/main/java/com/telran/qa/tests/LoginJira.java
+++ first_selenium_project/src/main/java/com/telran/qa/tests/LoginJira.java
@@ -3,6 +3,7 @@ package com.telran.qa.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -13,23 +14,39 @@ public class LoginJira {
     WebDriver wd;
 
     @BeforeClass
-    public void setUp(){
+    public void setUp() {
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wd.get("http://jira.tel-ran.net/secure/Dashboard.jspa");
+        // wd.manage().window().maximize();
+        // wd.get("http://jira.tel-ran.net/secure/Dashboard.jspa");
+        wd.navigate().to("http://jira.tel-ran.net/secure/Dashboard.jspa");
+
+
     }
 
-   @Test
+    @Test
     public void testLoginJira() throws InterruptedException {
-       click(By.name("os_username"));
-       wd.findElement(By.name("os_username")).clear();
-       wd.findElement(By.name("os_username")).sendKeys("ninav");
-       click(By.name("os_password"));
-       wd.findElement(By.name("os_password")).clear();
-       wd.findElement(By.name("os_password")).sendKeys("VsjoHorosho");
-       click(By.name("login"));
-       Thread.sleep(3000);
-   }
+
+        type(By.name("os_username"), "ninal");
+        type(By.name("os_password"), "VsjoHorosho");
+        click(By.name("login"));
+        Thread.sleep(3000);
+
+        Assert.assertTrue(isElementPresent(By.id("usernameerror")));
+        String errorMessage = wd.findElement(By.id("usernameerror")).getText();
+        Assert.assertEquals(errorMessage, "Sorry, your username and password are incorrect - please try again.");
+    }
+
+    public void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+    public boolean isElementPresent(By locator) {
+        return wd.findElements(locator).size() > 0;
+    }
+
 //   @Test
 //   public void testLogOut() throws InterruptedException {
 //        wd.findElement(By.className("aui-avatar aui-avatar-small")).click();
@@ -42,7 +59,7 @@ public class LoginJira {
     }
 
     @AfterClass
-    public void tearDown(){
-        wd.quit();
+    public void tearDown() {
+        //     wd.quit();
     }
 }
