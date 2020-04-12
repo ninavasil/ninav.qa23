080cbc38c9973f324d3c511eb00116fdc308763c
ninavasil <62669724+ninavasil@users.noreply.github.com>
2020-04-12 15:10:30 +0300

    last changes to hw

--- first_selenium_project/src/main/java/com/telran/qa/tests/LoginJira.java
+++ first_selenium_project/src/main/java/com/telran/qa/tests/LoginJira.java
@@ -3,6 +3,7 @@ package com.telran.qa.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -16,30 +17,45 @@ public class LoginJira {
     public void setUp(){
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wd.get("https://mail.ru/");
+     //   wd.get("http://jira.tel-ran.net/secure/Dashboard.jspa");
+        // wd.manage().window().maximize();
+        // wd.get("http://jira.tel-ran.net/secure/Dashboard.jspa");
+        wd.navigate().to("http://jira.tel-ran.net/secure/Dashboard.jspa");
 
     }
 
     @Test
     public void testLoginJira() throws InterruptedException {
+        type(By.name("os_username"), "ninav");
+        type(By.name("os_password"), "VsjoHorosho11");
         click(By.name("login"));
-        wd.findElement(By.name("login")).clear();
-        wd.findElement(By.name("login")).sendKeys("lika-ki");
-        click(By.className("o-control"));
-        click(By.name("password"));
-        wd.findElement(By.name("password")).clear();
-        wd.findElement(By.name("password")).sendKeys("VsjoHorosho");
-        click(By.className("o-control"));
         Thread.sleep(3000);
+
+        Assert.assertTrue(isElementPresent(By.id("usernameerror")));
+        String errorMessage = wd.findElement(By.id("usernameerror")).getText();
+        Assert.assertEquals(errorMessage, "Sorry, your username and password are incorrect - please try again.");
+
+    }
+    public void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
     }
 
+    public boolean isElementPresent(By locator) {
+        return wd.findElements(locator).size() > 0;
+    }
+
+//   @Test
+//   public void testLogOut() throws InterruptedException {
+//        wd.findElement(By.className("aui-avatar aui-avatar-small")).click();
 
     private void click(By locator) {
         wd.findElement(locator).click();
     }
 
     @AfterClass
-    public void tearDown(){
-        wd.quit();
+    public void tearDown() {
+        //     wd.quit();
     }
 }
---
+++ first_selenium_project/src/main/java/com/telran/qa/tests/hw_2.java
@@ -0,0 +1,49 @@
+package com.telran.qa.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class hw_2 {
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.get("http://jira.tel-ran.net/secure/Dashboard.jspa");
+    }
+
+    @Test
+    public void testLoginJira() throws InterruptedException {
+        click(By.name("os_username"));
+        wd.findElement(By.name("os_username")).clear();
+        wd.findElement(By.name("os_username")).sendKeys("ninav");
+        click(By.name("os_password"));
+        wd.findElement(By.name("os_password")).clear();
+        wd.findElement(By.name("os_password")).sendKeys("VsjoHorosho");
+        click(By.name("login"));
+        Thread.sleep(3000);
+    }
+//   @Test
+//   public void testLogOut() throws InterruptedException {
+//        wd.findElement(By.className("aui-avatar aui-avatar-small")).click();
+//        Thread.sleep(3000);
+//        click(By.id("log_out"));
+//   }
+
+    private void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    @AfterClass
+    public void tearDown(){
+        wd.quit();
+    }
+}
+
