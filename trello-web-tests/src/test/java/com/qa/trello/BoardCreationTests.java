6b7d87ac2c3cb91aeb9d8edc4057e35f2fca43db
ninavasil <62669724+ninavasil@users.noreply.github.com>
2020-04-17 16:50:12 +0300

    added Class TeamCreation

--- trello-web-tests/src/test/java/com/qa/trello/BoardCreationTests.java
+++ trello-web-tests/src/test/java/com/qa/trello/BoardCreationTests.java
@@ -26,23 +26,73 @@ public class BoardCreationTests {
     }
 
     @Test
-    public void testBoeardCreation(){
+    public void testBoardCreation() throws InterruptedException {
         clickLogin();
         fillLoginForm("lika-ki@mail.ru", "VsjoHorosho11");
         confirmLogin();
         CreateBoard();
         returnHome();
+        returnHome();
+        Thread.sleep(5000);
+        clickCreateTeam();
+        Thread.sleep(5000);
+        fillingForm("My Testteam");
+        Thread.sleep(5000);
+        chooseType();
+        writeDescription("This is my first created Team");
+        finishCreation();
     }
 
-    public void returnHome() {
+ //   @Test
+    public void teamCreation() throws InterruptedException {
+//        BoardCreationTests bct= new BoardCreationTests();
+//        bct.testBoardCreation();
+        clickCreateTeam();
+        fillingForm("My Testteam");
+        chooseType();
+        writeDescription("This is my first created Team");
+        finishCreation();
+    }
+
+    private void finishCreation() {
+        click(By.cssSelector("button[type='submit']"));
+    }
+
+    private void writeDescription(String text) {
+        type(By.cssSelector("textarea._15aIJYNKhrO4vB"),text);
+    }
+
+    private void chooseType() throws InterruptedException {
+       // click(By.cssSelector("div#teamTypeSelect"));
+       // Thread.sleep(5000);
+       // click(By.xpath("//input[@id='react-select-14-input']"));
+
+        click(By.xpath("//div[@class='css-dmzcl _1T1MG_BX1zeaq8'])"));
+        click(By.xpath("//div[@class='css-1wy0on6']"));
+        click(By.xpath("//div[contains(text(),'Education')]"));
+      //  click(By.cssSelector("div.css-1yqjdp6"));
+    }
+
+    private void clickCreateTeam() {
+        click(By.cssSelector("span.icon-add"));
+    }
+
+    public void fillingForm(String teamName) throws InterruptedException {
+            type(By.cssSelector("input._1CLyNodCAa-vQi"),teamName);
+
+    }
+
+    public void returnHome()  {
         click(By.cssSelector("[color='light']"));
     }
 
-    public void CreateBoard() {
+    public void CreateBoard() throws InterruptedException {
+        Thread.sleep(5000);
         click(By.name("add"));
         click(By.xpath("//button[@data-test-id='header-create-board-button']//./p[@class='_2DBw9GxD3tha0R']"));
         type(By.cssSelector("input[placeholder='Add board title']"),"myFirstBoard");
         click(By.xpath("//*[@data-test-id='create-board-submit-button']"));
+        Thread.sleep(5000);
     }
 
 
@@ -50,10 +100,14 @@ public class BoardCreationTests {
         click(By.cssSelector("[href='/login']"));
     }
 
-    public void fillLoginForm(String userEmail, String password) {
+    public void fillLoginForm(String userEmail, String password) throws InterruptedException {
+        Thread.sleep(5000);
         type(By.name("user"), userEmail);
         click(By.cssSelector("#login.button-green"));
-        type(By.cssSelector("input#password"), password);
+        type(By.xpath("//input[@class='Input__InputElement-sc-1o6bj35-0 bfCuIo']"), password);
+        //By.xpath("//input[@id='password']"
+        //By.cssSelector("input#current-password"
+
     }
 
     public void confirmLogin() {
---
+++ trello-web-tests/src/test/java/com/qa/trello/TestBase.java
@@ -0,0 +1,11 @@
+package com.qa.trello;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestBase {
+    WebDriver wd;
+
+}
+
+
